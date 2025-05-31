c85471a0e84a1f862522ef659c25921f76d12008
Feshchenko1 <114470355+Feshchenko1@users.noreply.github.com>
2025-05-31 17:38:30 +0300

    Create Jenkinsfile

--- src/test/java/com/dailycodework/pz_4_1/Pz41ApplicationTests.java
+++ src/test/java/com/dailycodework/pz_4_1/Pz41ApplicationTests.java
@@ -2,8 +2,9 @@ package com.dailycodework.pz_4_1;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@ActiveProfiles("test")
 class Pz41ApplicationTests {
 
     @Test
